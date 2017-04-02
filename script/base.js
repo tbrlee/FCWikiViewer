81b50f00ea7f09522e54eefa429dad26c6a27d65
Brian <tzhlee@gmail.com>
2017-04-01 23:50:48 -0500

    Wikiviewer 'version 1' completed

--- script/base.js
+++ script/base.js
@@ -31,17 +31,27 @@ $(function() {
         e.preventDefault();    
     });
    */
-    $('#search-btn').click(function() {
-        var searchFound = $('#query').val(); //Get search articles from search bar
+    $("#search-btn").click(function() {
+        var searchFound = $("#query").val(); //Get search articles from search bar
         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchFound+"&format=json&callback=?"; 
 
+        
         $.ajax({
             type:"GET",
             url:url,
             asynch:false,
             dataType: "json",
             success: function(data){
-                console.log(url);
+
+                /*
+                console.log(data[1][0]);
+                console.log(data[2][0]);
+                console.log(data[3][0]);
+                */
+                $("#results").empty();
+                for(var i=0; i < data[1].length; i++) {
+                $("#results").prepend("<li><a href= "+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
+                }
             },
             error: function(errorMessage) {
                 alert("Error");
